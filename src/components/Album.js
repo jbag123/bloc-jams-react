5fcc6bf8aa78d0e9a5baeecc3906bdf0b71df43d
Josh Cone <joshcone@googlemail.com>
2018-06-13 22:13:18 +0100

    completed checkpoint 7 buttons

--- src/components/Album.js
+++ src/components/Album.js
@@ -58,11 +58,12 @@ class Album extends Component {
     }
   }
 
-  handlePrevClick(song) {
-    const isSameSong = this.state.currentSong === song;
-    if (isSameSong) {
-      this.prev(song);
-    }
+  handlePrevClick() {
+    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+    const newIndex = Math.max(0, currentIndex - 1);
+    const newSong = this.state.album.songs[newIndex];
+    this.setSong(newSong);
+    this.play();
   }
 
   render() {
@@ -102,7 +103,7 @@ class Album extends Component {
           isPlaying={this.state.isPlaying}
           currentSong={this.state.currentSong}
           handleSongClick={() => this.handleSongClick(this.state.currentSong)}
-          handlePrevClick={() => this.handlePrevClick(this.state.currentSong)}
+          handlePrevClick={() => this.handlePrevClick()}
         />
       </section>
     );
