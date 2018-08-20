6bf3001d80834fdf941897b9b35082aea7643e58
Josh Cone <joshcone@googlemail.com>
2018-08-20 21:06:12 +0100

    commiting grey layout for bloc-jams

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { Route, Link } from 'react-router-dom';
 import './App.css';
 import './styles/main.css'
 import './styles/landing.css'
+import { Container, Nav, NavItem } from 'reactstrap';
+import { Row, Col } from 'reactstrap';
+import logo from './assets/images/bloc_jams_logo.png'
 import Landing from './components/Landing';
 import Library from './components/Library';
 import Album from './components/Album';
@@ -10,19 +13,40 @@ import Album from './components/Album';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header>
-          <nav className="navbar">
-            <Link to='/'>Landing</Link>
-            <Link to='/library'>Library</Link>
-          </nav>
-          <h1>Bloc Jams</h1>
-        </header>
-        <main>
+      <div>
+      <header>
+        <Row>
+          <Col sm="12" md={{ size: 6, offset: 3 }}>
+          <div className="clearfix">
+          <img alt="logo" className="float-left p-3" src={logo} />
+          <h1>Turn the music up!</h1>
+          </div>
+          </Col>
+        </Row>
+      </header>
+      <Container className="App">
+      <Row>
+      <Col xs="10">
+        <main className="h-100">
           <Route exact path="/" component={Landing} />
           <Route path="/library" component={Library} />
           <Route path="/album/:slug" component={Album} />
         </main>
+      </Col>
+      <Col xs="2">
+        <nav>
+          <Nav className="sidebar justified" vertical>
+            <NavItem className="navbar-brand pb-3">
+              <Link to='/'>Landing</Link>
+            </NavItem>
+            <NavItem className="navbar-brand pb-3">
+              <Link to='/library'>Library</Link>
+            </NavItem>
+          </Nav>
+        </nav>
+      </Col>
+      </Row>
+      </Container>
       </div>
     );
   }
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,25 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Ionicon from 'react-ionicons'
 
-const Landing = () => (
-  <section className="landing">
-    Landing page will go here
-    <h1 className="hero-title">Turn the music up!</h1>
 
-   <section className="selling-points">
-     <div className="point">
-       <h2 className="point-title">Choose your music</h2>
-       <p className="point-description">The world is full of music; why should you have to listen to music that someone else chose?</p>
-     </div>
-     <div className="point">
-       <h2 className="point-title">Unlimited, streaming, ad-free</h2>
-       <p className="point-description">No arbitrary limits. No distractions.</p>
-     </div>
-     <div className="point">
-       <h2 className="point-title">Mobile enabled</h2>
-       <p className="point-description">Listen to your music on the go. This streaming service is available on all mobile platforms.</p>
-     </div>
-   </section>
-  </section>
-);
+class Landing extends Component {
+    constructor(props) {
+    super(props);
+    this.compEl = React.createRef();
+  }
+  componentDidMount() {
+  	// Get the components DOM node
+    var elem = this.compEl.current;
+  	// Set the opacity of the element to 0
+  	elem.style.opacity = 0;
+  	window.requestAnimationFrame(function() {
+  		// Now set a transition on the opacity
+  		elem.style.transition = "opacity 2s";
+  		// and set the opacity to 1
+  		elem.style.opacity = 1;
+  	});
+  }
+
+    render() {
+     return (
+       <section className="d-flex flex-column h-100" ref={this.compEl}>
+          <div className="point flex-grow-1">
+            <Ionicon icon="ios-musical-notes" color="#00B212" fontSize="5em" />
+            <h2>Choose your music</h2>
+            <p>The world is full of music; why should you have to listen to music that someone else chose?</p>
+          </div>
+          <div className="point flex-grow-1">
+            <Ionicon icon="ios-radio-outline" color="#00B212" fontSize="5em" />
+            <h2>Unlimited, streaming, ad-free</h2>
+            <p>No arbitrary limits. No distractions.</p>
+          </div>
+          <div className="point flex-grow-1">
+            <Ionicon icon="ios-phone-portrait" color="#00B212" fontSize="5em" />
+            <h2>Mobile enabled</h2>
+            <p>Listen to your music on the go. This streaming service is available on all mobile platforms.</p>
+          </div>
+       </section>
+     );
+   }
+}
 
 export default Landing;
